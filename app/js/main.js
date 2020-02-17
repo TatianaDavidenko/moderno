85a4282e297d50e0df5892e2aea4a2405155fb92
TatianaDavidenko <logvinenko.tatiana@gmail.com>
2020-02-17 10:19:44 +0200

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
-$(function(){
+$(function () {
+
+  $(".rate-star").rateYo({
+    rating: 4.2,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3 
+
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 
- 
-    
 });
