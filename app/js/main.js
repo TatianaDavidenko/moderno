20ba021daec06b12c900872e0d1db7f854203cb4
TatianaDavidenko <logvinenko.tatiana@gmail.com>
2020-02-27 14:05:41 +0200

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -10,7 +10,7 @@ $(function () {
     dots: true,
     arrows: false,
     slidesToShow: 4,
-    slidesToScroll: 4 
+    slidesToScroll: 4
 
   });
 
@@ -21,20 +21,28 @@ $(function () {
     from: 0,
     to: 600,
     prefix: "$"
-});
+  });
 
-$('.icon-th-list').on('click', function(){
-  $('.products__item').addClass('list');
-  $('.icon-th-list').addClass('active');
-  $('.icon-th-large').removeClass('active')
-});
+  $('.icon-th-list').on('click', function () {
+    $('.products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active')
+  });
 
-$('.icon-th-large').on('click', function(){
-  $('.products__item').removeClass('list');
-  $('.icon-th-large').addClass('active');
-  $('.icon-th-list').removeClass('active')
-});
+  $('.icon-th-large').on('click', function () {
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active')
+  });
 
+
+  $('.menu__btn').on('click', function () {
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function () {
+    $('.header__box').toggleClass('active');
+  });
 
   var mixer = mixitup('.products__inner-box');
 
